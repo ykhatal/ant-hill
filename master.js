34110e25a4b42cebf6da2090f50acac158cd7080
ky23 <khatal.yacine@gmail.com>
2015-04-09 17:02:59 +0200

    cosmetic changes

--- master.js
+++ master.js
@@ -22,7 +22,7 @@ AntHill.prototype = {
   // Create server and listen to port
   createServer: function() {
     var self = this;
-    console.log('Server listening on ' + self.host + ':' + self.port);
+    console.log('Server listening on ' + this.host + ':' + this.port);
     net.createServer(function(socket) {
       var worker = self.addWorker(socket);
       // Called on data received
