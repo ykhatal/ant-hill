d8d522aedc40b59aa4cf931f7633cd2324047f9e
khatal yacine <khatal.yacine@gmail.com>
2015-04-11 18:12:22 +0200

    cosmetic change

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict';
 
 module.exports.QueenAnt = require('./lib/queen-ant');
-module.exports.WorkerAnt  = require('./lib/worker-ant');
+module.exports.WorkerAnt = require('./lib/worker-ant');
